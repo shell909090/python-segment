7a2d092c7ba922c1479c6b903aaf3e1c69666764
shell <shell@shell-deb>
2011-11-16 16:58:01 +0800

    * add utils.

---
+++ cutter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@date: 2011-11-16
+@author: shell.xu
+'''
+import os, sys
+import segment
+
+cut = segment.get_cutter('frq.db')
+
+def main():
+    with open(sys.argv[1], 'r') as fi: data = fi.read().decode('gbk')
+    print data
+    print '|'.join(cut.parse(data))
+    # list(cut.parse(data))
+
+if __name__ == '__main__': main()
--- dbmgr.py
+++ dbmgr.py
@@ -9,8 +9,13 @@ import segment
 
 dbname = 'frq.db'
 
-def load(filepath):
+def create(filepath):
     ddb = segment.dictdb(dbname, 'n')
+    ddb.loadtxt(filepath)
+    ddb.flush()
+
+def load(filepath):
+    ddb = segment.dictdb(dbname, 'w')
     ddb.loadtxt(filepath)
     ddb.flush()
 
@@ -28,16 +33,30 @@ def trans_cedict(infile, outfile):
         for word in list(words):
             fo.write((u'%s 1\n' % word).encode('utf-8'))
 
+def trans_plain(infile, outfile):
+    words = set()
+    with open(infile, 'r') as fi:
+        for line in fi:
+            if line.startswith('#'): continue
+            word = line.decode('utf-8').strip()
+            if len(word) > 1: words.add(word)
+    with open(outfile, 'w') as fo:
+        for word in list(words):
+            fo.write((u'%s 1\n' % word).encode('utf-8'))
+
 def main():
     opts, args = getopt.getopt(sys.argv[1:], 'd:')
     for opt, val in opts:
         if opt == '-d':
             global dbname
             dbname = val
+    funcs = ['create', 'load', 'export', 'trans_cedict', 'trans_plain']
     if len(args) == 0:
-        print 'loadtxt or exporttxt'
+        print '%s [%s] .. filename' % (sys.argv[0], '|'.join(funcs))
+    elif args[0] == 'create': create(args[1])
     elif args[0] == 'load': load(args[1])
     elif args[0] == 'export': export(args[1])
     elif args[0] == 'trans_cedict': trans_cedict(args[1], args[2])
+    elif args[0] == 'trans_plain': trans_plain(args[1], args[2])
 
 if __name__ == '__main__': main()
