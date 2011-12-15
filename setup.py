e17a770ff76933b334ffcc1b8b51923e346fbfe5
shell <shell@debox>
2011-12-15 16:53:21 +0800

    * update ps_cls in setup.

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ from distutils.core import setup
 setup(name = 'segment', version = '1.0', url = 'http://shell909090.com/',
       author = 'Shell.E.Xu', author_email = 'shell909090@gmail.com',
       description = 'segmentation library written by python',
-      packages = ['segment',], scripts=['ps_cutter', 'ps_dbmgr'])
+      packages = ['segment',], scripts=['ps_cutter', 'ps_dbmgr', 'ps_cls'])
